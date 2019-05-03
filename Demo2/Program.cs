1422a5cfffd236900ae0fbd2a33fb5cf341dff02
Perry Lambrechts <p.lambrechts@ettu.nl>
2019-05-03 14:36:49 +0200

    Initial version

---
+++ Demo2/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Azure.Documents.Client;
+using System;
+using System.Reflection;
+
+namespace Demo2
+{
+    internal class Program
+    {
+        private static readonly string cosmosURI = "https://localhost:8081";
+        private static readonly string cosmosPrimaryKey = "<your key goes here>";
+        private static readonly string cosmosDatabaseId = "MyDatabase";
+        private static readonly string cosmosCollectionId = "MyCollection";
+
+        private static void Main(string[] args)
+        {
+            using (DocumentClient cosmosClient = new DocumentClient(new Uri(cosmosURI), cosmosPrimaryKey))
+            {
+                Uri collectionUri = UriFactory.CreateDocumentCollectionUri(cosmosDatabaseId, cosmosCollectionId);
+                string docPartition = Assembly.GetExecutingAssembly().GetName().Name;
+
+                var timestamp = new DateTime(2019, 3, 27, 18, 15, 0, DateTimeKind.Unspecified);
+
+                User[] users = new User[]
+                {
+                    new User() { id = "1", partition = docPartition, UserName = "User1", LastLogin = timestamp },
+                    new User() { id = "2", partition = docPartition, UserName = "User2", LastLogin = timestamp.ToLocalTime() },
+                    new User() { id = "3", partition = docPartition, UserName = "User3", LastLogin = timestamp.ToUniversalTime() }
+                };
+
+                foreach (User user in users)
+                    cosmosClient.CreateDocumentAsync(collectionUri, user).Wait();
+            };
+        }
+
+        private class User
+        {
+            public string id;
+            public string partition;
+            public string UserName;
+            public DateTimeOffset LastLogin;
+        }
+    }
+}
---
+++ Demo3/Program.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Azure.Documents.Client;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+
+namespace Demo3
+{
+    internal class Program
+    {
+        private static readonly string cosmosURI = "https://localhost:8081";
+        private static readonly string cosmosPrimaryKey = "<your key goes here>";
+        private static readonly string cosmosDatabaseId = "MyDatabase";
+        private static readonly string cosmosCollectionId = "MyCollection";
+
+        private static void Main(string[] args)
+        {
+            using (DocumentClient cosmosClient = new DocumentClient(new Uri(cosmosURI), cosmosPrimaryKey))
+            {
+                Uri collectionUri = UriFactory.CreateDocumentCollectionUri(cosmosDatabaseId, cosmosCollectionId);
+                string docPartition = Assembly.GetExecutingAssembly().GetName().Name;
+
+                var timestamp = new DateTime(2019, 3, 27, 18, 15, 0, DateTimeKind.Unspecified);
+
+                User[] users = new User[]
+                {
+                    new User() { id = "1", partition = docPartition, UserName = "User1", LastLogin = timestamp },
+                    new User() { id = "2", partition = docPartition, UserName = "User2", LastLogin = timestamp.ToLocalTime() },
+                    new User() { id = "3", partition = docPartition, UserName = "User3", LastLogin = timestamp.ToUniversalTime() }
+                };
+
+                JsonConvert.DefaultSettings = () => new JsonSerializerSettings()
+                {
+                    Converters = new List<JsonConverter>()
+                    {
+                        {
+                            new IsoDateTimeConverter()
+                            {
+                                DateTimeStyles = DateTimeStyles.AdjustToUniversal
+                            }
+                        }
+                    }
+                };
+
+                foreach (User user in users)
+                    cosmosClient.CreateDocumentAsync(collectionUri, user).Wait();
+            };
+        }
+
+        private class User
+        {
+            public string id;
+            public string partition;
+            public string UserName;
+            public DateTimeOffset LastLogin;
+        }
+    }
+}
